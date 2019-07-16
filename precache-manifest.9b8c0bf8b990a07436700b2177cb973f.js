04d3a5f904bc8d193de97d161f946fb695e2f573
HappyDev86 <cmalanda2302@gmail.com>
2019-07-16 17:47:26 +0200

    Updates

---
+++ precache-manifest.9b8c0bf8b990a07436700b2177cb973f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b70629bd2197058b7942bf5376a57d2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "63ff8e9c31dbf4ba935e",
+    "url": "/monsters-rolodex/static/css/main.c27dc21e.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "63ff8e9c31dbf4ba935e",
+    "url": "/monsters-rolodex/static/js/main.6c5834fc.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
